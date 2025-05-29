a9dfa210f66283f13bb8313ce2af0ea3b222d7f7
addthoriq <alvarro.thoriq@gmail.com>
2025-05-29 11:46:42 +0700

    Learn FiberGO

---
+++ routes/user.go
@@ -0,0 +1,11 @@
+package routes
+
+import "github.com/gofiber/fiber/v2"
+
+func UserRoutes(app *fiber.App) {
+	user := app.Group("/user")
+
+	user.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{"message": "Anda di /user"})
+	})
+}
